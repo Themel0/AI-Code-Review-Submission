6008e3ba14fbb681582447123ba059d03cafb67b
Melike Uysal <melikeuysal778@gmail.com>
2026-01-17 09:56:51 +0300

    corrected task1

--- correct_task1.py
+++ correct_task1.py
@@ -1,2 +1,16 @@
 # Write your corrected implementation for Task 1 here.
 # Do not modify `task1.py`.
+def calculate_average_order_value(orders):
+    total = 0
+    count = len(orders)
+
+    if count > 0:
+        for order in orders:
+            status = order.get("status")
+            amount = order.get("amount")
+            if status != "cancelled":
+                total += amount
+
+        return total/count
+
+    return 0
