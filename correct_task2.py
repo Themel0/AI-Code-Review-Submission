bcebf83a8c475fff99f2e30696236b9ccf5b17e2
Melike Uysal <melikeuysal778@gmail.com>
2026-01-17 10:12:16 +0300

    corrected task2

--- correct_task2.py
+++ correct_task2.py
@@ -1,2 +1,18 @@
 # Write your corrected implementation for Task 2 here.
 # Do not modify `task2.py`.
+def count_valid_emails(emails):
+    count = 0
+    for email in emails:
+        if email.count("@") != 1:
+            continue
+        username, domain = email.split("@")
+
+        if not username or not domain:
+            continue
+
+        if "." not in domain:
+            continue
+
+        count += 1
+
+    return count
